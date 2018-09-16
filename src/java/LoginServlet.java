e2a340a4a3552baf30f00a1c6577b369face8264
Cheng Yuan <cy1818cy@bupt.edu.cn>
2018-09-16 16:28:16 +0800

    java tbdddddaefs asdfasdfdsaaesasdadsfasd

---
+++ src/java/LoginServlet.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Administrator
+ */
+@WebServlet(urlPatterns = {"/login"})
+public class LoginServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if(request.getParameterValues("name") == null || request.getParameterValues("password") == null){
+            response.sendRedirect("/JavaShadowInk/login.html");
+            return;
+        }
+        String name, password;
+        name = request.getParameterValues("name")[0];
+        password = request.getParameterValues("password")[0];
+        getServletContext().setAttribute("hellomessage", "登陆失败！");
+        response.sendRedirect("/JavaShadowInk/loginFail.jsp");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- web/loginFail.jsp
+++ web/loginFail.jsp
@@ -4,7 +4,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <title>登陆 - 墨影</title>
+    <title>?? - ??</title>
     <meta id="viewport" name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,minimal-ui">
     <link rel="stylesheet" href="./login_files/login.css">	
     <script type="text/javascript" src="./login_files/patternLock.min.js" charset="utf-8"></script>
@@ -40,34 +40,34 @@
     <section class="avatar-wrapper" id="avatarWrapper">
 	<img src="./image/icon.png">
 	</section>
-    <form id="loginForm" action="/login" method="POST">
+    <form id="loginForm" action="/JavaShadowInk/slogin" method="POST">
         <div class="box">
             <div class="input-wrapper">
                 <i class="icon forName"></i>
                 <p class="input-box">
-                    <input type="text" placeholder="用户名/手机号" id="loginName" name="name">
+                    <input type="text" placeholder="???/???" id="loginName" name="name">
                     <a href="javascript:;" class="input-clear hid" id="loginnameclear"></a>
                 </p>
             </div>
             <div class="input-wrapper">
                 <i class="icon forPwd"></i>
                 <p class="input-box">
-                <input type="password" placeholder="请输入密码" id="loginPassword" name="password">
+                <input type="password" placeholder="?????" id="loginPassword" name="password">
                 </p>
             </div>
         </div>
 		
 		<div id="ToastDiv" style=" height:30px; width:100%;">
 			<script>
-				Toast( "{{ HelloMessage }}" ,2000)
+				Toast( "${HelloMessage}" ,2000)
 			</script>
 		</div>
-        <a href="javascript:document.getElementById('loginForm').submit()" class="btn btnRed" id="loginAction">登陆</a>
+        <a href="javascript:document.getElementById('loginForm').submit()" class="btn btnRed" id="loginAction">??</a>
         
     </form>
     <footer class="footer">
-        <a href="/register">注册账号</a>
-        <a href="">忘记密码</a>
+        <a href="/JavaShadowInk/register">????</a>
+        <a href="">????</a>
     </footer>
 </body>
 </html>
